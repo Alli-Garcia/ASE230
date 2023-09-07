04f9b9bbcaba76722f4b556ba826133cd74e7751
Cody <cody.king04@outlook.com>
2023-09-06 22:06:23 -0400

    Changed detail page to loop through awards to add awards to page

--- 02/detail.php
+++ 02/detail.php
@@ -617,21 +617,18 @@ $members = [
 							<h2 class="resume-section-title text-uppercase font-weight-bold pb-3 mb-3">Awards</h2>
 							<div class="resume-section-content">
 								<ul class="list-unstyled resume-awards-list">
-									<li class="mb-2 ps-4 position-relative">
-										<i class="resume-award-icon fas fa-trophy position-absolute"
-											data-fa-transform="shrink-2"></i>
-										<div class="resume-award-name">Award Name Lorem</div>
-										<div class="resume-award-desc">Award desc goes here, ultricies nec, pellentesque
-											eu, pretium quis, sem. Nulla consequat massa quis enim. Donec pede justo.
-										</div>
-									</li>
-									<li class="mb-0 ps-4 position-relative">
-										<i class="resume-award-icon fas fa-trophy position-absolute"
-											data-fa-transform="shrink-2"></i>
-										<div class="resume-award-name">Award Name Ipsum</div>
-										<div class="resume-award-desc">Award desc goes here, ultricies nec,
-											pellentesque.</div>
-									</li>
+									<?php
+										// Loop that goes through awards array for member and adds award item for each award.
+										for ($i = 0; $i < count($members[$_GET["index"]]['awards']); $i++) {
+											echo
+												'<li class="mb-0 ps-4 position-relative">
+													<i class="resume-award-icon fas fa-trophy position-absolute"
+														data-fa-transform="shrink-2"></i>
+												<div class="resume-award-name">' . $members[$_GET["index"]]['awards'][$i]['name'] . '</div>
+												<div class="resume-award-desc">' . $members[$_GET["index"]]['awards'][$i]['description'] . '</div>
+												</li>';
+										}
+									?>
 								</ul>
 							</div>
 						</section><!--//interests-section-->
